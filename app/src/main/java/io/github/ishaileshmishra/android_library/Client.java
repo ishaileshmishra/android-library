1d1e5008c53edccdaa95e6960f3ce2a63298aef9
Shailesh Mishra <mshaileshr@gmail.com>
2023-06-20 12:08:25 +0530

    build process

---
+++ app/src/main/java/io/github/ishaileshmishra/android_library/Client.java
@@ -0,0 +1,17 @@
+package io.github.ishaileshmishra.android_library;
+
+public class Client {
+
+    public String API_KEY;
+    public String AUTH_TOKEN;
+
+    Client(String API_KEY) {
+        return;
+    }
+
+    public Client(String API_KEY, String AUTH_TOKEN) {
+
+    }
+
+
+}
